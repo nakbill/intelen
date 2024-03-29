8d7333a4b46633384d274bf2efc8163823d12144
Vasilis Nakos <basilis.nakos@gmail.com>
2024-03-29 11:56:35 +0200

    - Add spinner

---
+++ docker/spinner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+spinner()
+{
+    local pid=$!
+    local delay=0.1
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+# Start spinner animation
+spinner &
